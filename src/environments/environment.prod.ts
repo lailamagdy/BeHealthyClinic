ab34386d79ed33f2e4ad8e46dbbf1a0aed53e479
lailamagdy <lailamagdy0@gmail.com>
2019-01-20 09:01:35 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
 };
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBXh3NSTZSdvtKSjp3QMDN2xxseDur8dR8",
+  authDomain: "firstapp-b872b.firebaseapp.com",
+  databaseURL: "https://firstapp-b872b.firebaseio.com",
+  projectId: "firstapp-b872b",
+  storageBucket: "firstapp-b872b.appspot.com",
+  messagingSenderId: "84629758048"
+};
